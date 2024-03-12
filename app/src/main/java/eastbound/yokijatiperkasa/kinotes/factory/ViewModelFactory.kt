b242b81d044ed83c296b7a0fff7be7ed97902c5f
Yoki Jati Perkasa <yokijati@gmail.com>
2024-03-12 19:58:20 +0700

    Berhasil membuat Insert Activity

---
+++ app/src/main/java/eastbound/yokijatiperkasa/kinotes/factory/ViewModelFactory.kt
@@ -0,0 +1,13 @@
+package eastbound.yokijatiperkasa.kinotes.factory
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import eastbound.yokijatiperkasa.kinotes.repository.Repository
+import eastbound.yokijatiperkasa.kinotes.ui.detail.DetailViewModel
+import eastbound.yokijatiperkasa.kinotes.ui.insert.InsertViewModel
+import eastbound.yokijatiperkasa.kinotes.ui.main.MainViewModel
+
+
+
+
